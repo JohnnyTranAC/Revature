4ab393ad298f2e06daec2104354987f497e4c650
Johnny Tran <74635740+JohnnyTranAC@users.noreply.github.com>
2022-06-27 22:23:50 -0700

    Create W1_YourNameIsMine.js

---
+++ ProjectOnboarding/WeeklyCodingChallenges/W1_YourNameIsMine.js
@@ -0,0 +1,17 @@
+// 6/27 - 7/1/2022
+// Q2: Your Name is Mine
+//   In an attempt to control the rise in population, Archer was asked to come up with a plan. This time he is targeting marriages.
+//   Archer, being as intelligent as he is, came up with the following plan:
+//     A man with name M is allowed to marry a woman with name W, only if M is a subsequence of W or W is a subsequence of M.
+//     A is said to be a subsequence of B, if A can be obtained by deleting some elements of B without changing the order of the remaining elements.
+//   Your task is to determine whether a couple is allowed to marry or not, according to Archer's rule.
+
+function isSpouse(T, M, W) {
+  
+}
+
+// INPUT
+
+
+// OUTPUT
+console.log(isSpouse(T, M, W));
