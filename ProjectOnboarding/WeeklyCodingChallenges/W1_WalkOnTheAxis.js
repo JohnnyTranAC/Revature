43b16b2d8c1ad0a097e5668c2e2677d2f8da7736
Johnny Tran <74635740+JohnnyTranAC@users.noreply.github.com>
2022-06-28 07:58:07 -0700

    Update W1_WalkOnTheAxis.js

--- ProjectOnboarding/WeeklyCodingChallenges/W1_WalkOnTheAxis.js
+++ ProjectOnboarding/WeeklyCodingChallenges/W1_WalkOnTheAxis.js
@@ -7,11 +7,37 @@
 //   You want to know how much distance you walked in the process. Note that distance between (a,0) and (b,0) is |a-b|.
 
 function findDist(T, N) {
-  
+    var result = new Array(T);
+    
+    for (let i=0; i<T; i++) {
+        // Create axis of lights
+        var lightAxis = [];
+        for (let j=0; j<=N[i]; j++) {
+            lightAxis.push(j);
+        }
+        // Initial movement from leftmost (0, 0) to rightmost light (N, 0)
+        result[i] = Math.abs(lightAxis[0] - lightAxis[lightAxis.length-1]);
+        
+        var moveLeft = 1;
+        while (lightAxis.length > 1) {
+            // Calculate distance when moving from one side to another
+            // Then remove that light from the axis
+            if (moveLeft == 1) {
+                result[i] += lightAxis[lightAxis.length-1] - lightAxis[0];
+                lightAxis.pop();
+            } else {
+                result[i] += Math.abs(lightAxis[0] - lightAxis[lightAxis.length-1]);
+                lightAxis.shift();
+            }
+        }
+    }
+    
+    return result.join('\n');
 }
 
 // INPUT
-
+var T = 2;
+var N = [1, 2];
 
 // OUTPUT
 console.log(findDist(T, N));
